b34233ce0e969f4c2d5c554c90fce573e3acd377
LucasTiriva <lucastiriva@gmail.com>
2024-06-20 17:45:48 -0300

    Dando sequencia nas aulas lidando mais com JS

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 61;
+console.log(`a velocidade é ${vel}km/h`);
+
+if (vel > 60) {
+    console.log(`Está acima da velocidade, MULTADO!`);
+}
---
+++ ex012.js
@@ -0,0 +1,11 @@
+var hora = 5;
+console.log(`Agora são ${hora} horas.`);
+if (hora == 0 || hora <= 4) {
+    console.log('boa madrugada')
+}else if (hora < 12) {
+    console.log('bom dia');
+} else if (hora <= 18) {
+    console.log('Boa tarde');
+} else if (hora <= 23 ) {
+    console.log('Boa noite');
+}
