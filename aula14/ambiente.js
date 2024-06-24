646221ed5043f0c2b68919866dfd9d1cde0da41d
LucasTiriva <lucastiriva@gmail.com>
2024-06-24 18:09:23 -0300

    Trabalhando com repetições e iniciando em arrays.

---
+++ aula14/ambiente.js
@@ -0,0 +1,6 @@
+for (var c = 1; c < 5 ;c++) {
+    console.log(c)
+}
+console.log('acabou')
+//notei que o valor de c não é 5 quando está "<=", para que o valor permanece igual a 5 preciso deixar somente "<".
+console.log(c)
