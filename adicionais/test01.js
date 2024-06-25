fe909308d597b13fbd83cfd86ccc10f0aa24c510
LucasTiriva <lucastiriva@gmail.com>
2024-06-25 18:01:39 -0300

    Usando o GPT para fazer alguns exercicios

---
+++ adicionais/test01.js
@@ -0,0 +1,43 @@
+//Exercico 01 do dia 01
+
+let nome = "João";
+let idade = "30";
+
+console.log(`Meu nome é ${nome} e tenho ${idade} anos`)
+
+
+//Exercico 02 do dia 01
+/**********************************/
+let carro = {marca: "Gol", modelo: "quadrado", ano: 2020};
+
+console.log(carro.marca);
+console.log(carro.modelo);
+console.log(carro.ano);
+
+
+//Exercico 03 do dia 01
+/***********************************/
+let cores = ['vermelho', 'azul', 'verde'];
+
+cores.push('rosa');
+cores.shift();
+console.log(cores)
+
+
+//Exercico 04 do dia 01
+/***********************************/
+//esse exercicio eu não entendi direito, mas tentei o que eu entendi :)
+/*function num(a, b) {
+    return a + b;
+    console.log(num)
+}*/
+
+
+
+//Exercico 05 do dia 01
+/***********************************/
+let igual = (10 == '10');
+console.log(igual);
+
+let igualMesmo = (10 === '10');
+console.log(igualMesmo);
